262ebe05333452c6b8fb787e0cbb7fdca034ea9e
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-09 14:19:07 -0400

    Add Client class

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,12 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public string CellPhoneNum { get; set; }
+    public string Notes { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
+  }
+}
