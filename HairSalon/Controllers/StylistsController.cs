ba9fa343e6d2d82325f8a15d4e12ab8e2e16c7d0
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-09 19:18:11 -0400

    Add route for capturing user input from form to edit a stylist

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -45,5 +45,13 @@ namespace HairSalon.Controllers
       var thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
       return View(thisStylist);
     }
+
+    [HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Entry(stylist).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
